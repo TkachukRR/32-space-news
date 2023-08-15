7d82ddb375275a27f791a6778207dfd7ba803be8
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-15 16:30:51 +0200

    Generated articles-board component

---
+++ src/app/components/articles-board/articles-board.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articles-board',
+  templateUrl: './articles-board.component.html',
+  styleUrls: ['./articles-board.component.scss']
+})
+export class ArticlesBoardComponent {
+
+}
