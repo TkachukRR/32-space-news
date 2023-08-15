1efccd94c42913f3f04e20fdac8e5f064775eedf
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-15 16:27:19 +0200

    Added ability get articles

--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { map, Observable, tap } from "rxjs";
+
+const API = {
+  protocol: 'https',
+  url: 'api.spaceflightnewsapi.net',
+  version: 'v4',
+  service: 'articles'
+}
+
+export interface ApiResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Article[];
+}
+
+export interface Article{
+  id: number;
+  image_url: string;
+  title: string;
+  summary: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService {
+  private _http = inject(HttpClient);
+  private _articlesUrl =`${API.protocol}://${API.url}/${API.version}/${API.service}/`
 
+  public getArticles(): Observable<Article[]> {
+    return this._http.get<ApiResponse>(this._articlesUrl)
+      .pipe(
+        map((resp: ApiResponse) => resp.results)
+      )
+  }
 }
