5f99ca12d5ca6f7bd10866a2190b850fd4cdb57f
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-17 14:17:22 +0200

    Generated article card component

---
+++ src/app/components/article-card/article-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Article } from "../../services/articles.service";
+
+@Component({
+  selector: 'app-article-card',
+  templateUrl: './article-card.component.html',
+  styleUrls: ['./article-card.component.scss']
+})
+export class ArticleCardComponent {
+  @Input() articleData!: Article;
+}
