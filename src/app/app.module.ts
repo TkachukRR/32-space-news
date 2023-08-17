02bb5f8bf0271a878f7afdf37b0aa17f8f8a3f82
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-17 14:18:38 +0200

    Changed from new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ArticlesBoardComponent } from './components/articles-board/articles-board.component';
 import { MatInputModule } from "@angular/material/input";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { ArticlesBoardComponent } from './components/articles-board/articles-board.component';
+import { ArticleCardComponent } from "./components/article-card/article-card.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticlesBoardComponent
+    ArticlesBoardComponent,
+    ArticleCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatInputModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
