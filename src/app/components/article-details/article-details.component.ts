0dad361c79a46bb8fb994d1be8fda25238790543
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-18 12:36:54 +0200

    Added ability get article detail in component

--- src/app/components/article-details/article-details.component.ts
+++ src/app/components/article-details/article-details.component.ts
@@ -1,10 +1,22 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { Article, ArticlesService } from "../../services/articles.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-article-details',
   templateUrl: './article-details.component.html',
   styleUrls: ['./article-details.component.scss']
 })
-export class ArticleDetailsComponent {
-  @Input() articleId!: number;
+export class ArticleDetailsComponent implements OnInit{
+  private _as = inject(ArticlesService);
+  private _route = inject(ActivatedRoute);
+  @Input() articleId: number = Number(this._route.snapshot.paramMap.get('articleID'));
+  public article: Article = {} as Article
+
+  public ngOnInit(): void {
+    this._as.articles$.subscribe(
+      (articles: Article[]) =>
+        this.article = articles.filter((article: Article) => article.id === this.articleId)[0]
+    )
+  }
 }
